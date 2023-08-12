4bab390b89ebee81b89a79382e2d48a77c57158c
liuxixin <2658030236@qq.com>
2023-08-12 12:57:34 +0800

    上传停止脚本

---
+++ stop.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo  -e "\033[34m--\033[0m"
+
+ps -ef |grep pn_uwsgi.ini | grep -v grep
+
+sleep 1
+
+echo  -e "\033[34m--\033[0m"
+
+ps -ef |grep pn_uwsgi.ini | grep -v grep | awk '{print $2}' |  | xargs kill -9
+
+sleep 1
