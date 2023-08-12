51fc88b00d1736bbd9868e12e9e035d86d144569
liuxixin <2658030236@qq.com>
2023-08-12 13:06:12 +0800

    加一下打印

--- stop.sh
+++ stop.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo  -e "\033[34m--\033[0m"
+echo  -e "\033[34m-------------------------uwsgi stop---------------------------\033[0m"
 
 ps -ef |grep pn_uwsgi.ini | grep -v grep
 
 sleep 1
 
-echo  -e "\033[34m--\033[0m"
+echo  -e "\033[34m-------------------------run stop------------------------------\033[0m"
 
 ps -ef |grep pn_uwsgi.ini | grep -v grep | awk '{print $2}' | xargs kill -9
 
